3235985da3455a565d18723c83c39c2fbe47ed77
Kai-Taka <50423776+Kai-Taka@users.noreply.github.com>
2025-06-01 18:25:39 -0300

    implementação de cinematica inversa - Autor: Arthur

--- scara.py
+++ scara.py
@@ -16,8 +16,48 @@ robot = rtb.DHRobot(links, name='Robo SCARA')
 # Exibe os parâmetros DH do robô
 print(robot)
 
-# Define a configuração inicial das juntas do robô
-initialPos = [0, 0, 27.5, 0, 0, 0]  # Posição inicial com segunda junta rotacional a 45 graus
 
-# Plota o robô com sistemas de coordenadas, vetores das juntas, base e efetuador final
-robot.teach(initialPos)
+def Cinematica_direta():
+    initialPos = [0, 0, 27.5, 0, 0, 0]
+    robot.teach(initialPos)
+
+
+def Cinematica_inversa():
+    
+        # Define a posição do efetuador final
+    x = float(input("Digite a coordenada x do efetuador final: "))
+    y = float(input("Digite a coordenada y do efetuador final: "))
+    z = float(input("Digite a coordenada z do efetuador final: "))
+
+    #Se posição se encontra dentro do raio do robo
+    if (np.sqrt(x**2+y**2) < 28):
+        d1 = 0
+    else: 
+        d1 = x - np.sqrt(28**2-y**2)
+
+    Theta2 = np.arccos(((x-d1)**2 + y**2 - 17**2 - 11**2)/(2*17*11) )  # Calcula o ângulo da segunda junta
+
+
+    Theta1 = np.arctan2(y, x - d1) - np.arctan2(11 * np.sin(Theta2), 17 + 11 * np.cos(Theta2)) # Calcula o ângulo da primeira junta
+
+    d2 = 27.5 - z  # Calcula a distância da terceira junta prismática ao efetuador final
+    # Calcula a posição das juntas do robô
+
+    print("Posição das juntas do robô:")
+    print(f"Junta 1 (d1): {d1} cm") 
+    print(f"Junta 2 (Theta1): {np.degrees(Theta1)} graus")
+    print(f"Junta 3 (Theta2): {np.degrees(Theta2)} graus")
+    print(f"Junta 4 (d2): {d2} cm") 
+
+    # Importa a biblioteca necessária para o robô SCARA
+
+    # Define a configuração inicial das juntas do robô
+    initialPos = [0, d1, 27.5, Theta1, Theta2, d2]  # Posição inicial com segunda junta rotacional a 45 graus
+
+    # Plota o robô com sistemas de coordenadas, vetores das juntas, base e efetuador final
+    robot.teach(initialPos)
+
+if input("Pressione 'i' para calcular a Cinemática Inversa do robô SCARA...") == "i":
+    Cinematica_inversa()
+else:
+    Cinematica_direta()
